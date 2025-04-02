4bd69b85e9f851eec6ad8496a5e9af89072863d6
swathireddy369 <swathiamaravadi53@gmail.com>
2025-04-02 14:56:39 -0400

    initial commit

--- Documents/Spring-2025/Project/business_intelligence_dashboard_server/src/main/java/com/example/business_intelligence_dashboard_server/BusinessIntelligenceDashboardServerApplication.java
+++ Documents/Spring-2025/Project/business_intelligence_dashboard_server/src/main/java/com/example/business_intelligence_dashboard_server/BusinessIntelligenceDashboardServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusinessIntelligenceDashboardServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hiii");
 		SpringApplication.run(BusinessIntelligenceDashboardServerApplication.class, args);
 	}
 
